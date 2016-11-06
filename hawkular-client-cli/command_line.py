2f3195e833523ac8a300e65446dbaeafe2ec97b1
yaacov <kobi.zamir@gmail.com>
2016-11-06 15:23:03 +0200

    initial commit

---
+++ hawkular-client-cli/command_line.py
@@ -0,0 +1,102 @@
+"""
+    Copyright 2016 Red Hat, Inc. and/or its affiliates
+    and other contributors.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+import os
+import ssl
+import argparse
+import yaml
+import ssl
+from future.moves.urllib.parse import urlparse
+from hawkular.metrics import HawkularMetricsClient, MetricType
+
+# Read cli arguments
+def get_args():
+    parser = argparse.ArgumentParser(description='Read/Write data to and from a Hawkular metric server.')
+    parser.add_argument('-n', '--url', dest='url', type=str, nargs='?',
+                        help='Hawkualr server url')
+    parser.add_argument('-i', '--insecure', action='store_true',
+                        help='allow insecure ssl connection')
+    parser.add_argument('-t', '--tenant', metavar='NAME', type=str,
+                        help='Hawkualr tenenat name')
+    parser.add_argument('-c', '--config', dest='config_file', type=str, nargs='?',
+                        default='/etc/hawkular-client-cli/conifg.yaml',
+                        help='Configurations file path')
+    parser.add_argument('-p', '--password', dest='password', type=str, nargs='?',
+                        help='Hawkualr server password')
+    parser.add_argument('-u', '--username', dest='username', type=str, nargs='?',
+                        help='Hawkualr server username')
+    parser.add_argument('values', metavar='KEY=VALUE', type=str, nargs='*',
+                        help='key value pairs to send')
+    parser.add_argument('-a', '--tags', metavar='TAG=VALUE', dest='tags', type=str, nargs='*',
+                        help='a list of tags to update')
+    parser.add_argument('-k', '--keys', metavar='KEY', dest='keys', type=str, nargs='*',
+                        help='a list of keys to update')
+    parser.add_argument('-l', '--list', action='store_true',
+                        help='list all registered keys')
+    parser.add_argument('-r', '--read', metavar='KEY', type=str, nargs='+',
+                        help='read data for keys')
+    return parser.parse_args()
+
+# Read config file
+def get_config(config_file):
+    config = {}
+    if os.path.exists(config_file):
+        config = yaml.load(file(config_file))
+    config['hawkular'] = config.get('hawkular', {})
+    config['tags'] = config.get('tags', [])
+
+    return config
+
+# Get Hawkular server and credentials
+def get_client(args, config):
+    url = args.url or config.get('hawkular').get('url') or None
+    tenant = args.tenant or config.get('hawkular').get('tenant') or None
+    username = args.username or config.get('hawkular').get('username') or None
+    password = args.password or config.get('hawkular').get('password') or None
+    context = ssl._create_unverified_context() if args.insecure else None
+
+    url_args = urlparse(url)
+    return HawkularMetricsClient(host=url_args.hostname, port=url_args.port, 
+                                 scheme=url_args.scheme, username=username, password=password, 
+                                 tenant_id=tenant, context=context)
+
+def main():
+    # Get client, config and args
+    args = get_args()
+    config = get_config(args.config_file)
+    client = get_client(args, config)
+
+    # Do actions
+    if args.list:
+        definitions = client.query_metric_definitions()
+        for definition in definitions:
+            print('id:  ', definition['id'])
+            print('tags:', definition['tags'])
+
+    # Do actions
+    if args.read:
+        for key in args.read:
+            print(key)
+            values = client.query_metric(MetricType.Gauge, key, limit=10)
+            for v in values:
+                print (v['timestamp'], v['value'])
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='hawkular-client-cli',
+    version='0.1.0',
+    description='Hawkular client cli',
+    long_description='Hawkular client command line tool.',
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: System :: Monitoring',
+    ],
+    url='http://github.com/yaacov/hawkular-client-cli',
+    author='Yaacov Zamir',
+    author_email='yzamir@redhat.com',
+    license='Apache License 2.0',
+    packages=['hawkular-client-cli'],
+    install_requires=[
+    'future',
+    'yaml',
+    'hawkular-client-python',
+    ],
+    entry_points={
+    'console_scripts': ['hawkular-client-cli.command_line:main'],
+    },
+    include_package_data=True,
+    zip_safe=False)
