22ce25f3f9c0860b56649bc50fca0916bad6a84b
yaacov <yzamir@redhat.com>
2017-04-12 10:04:34 +0300

    Bug bug version

--- hawkular_client_cli/command_line.py
+++ hawkular_client_cli/command_line.py
@@ -18,7 +18,7 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-_VERSION = '0.18.0'
+_VERSION = '0.18.1'
 _DESCRIPTION = 'Read/Write data to and from a Hawkular metric server.'
 
 import os
